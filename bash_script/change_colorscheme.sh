5e930b4fcde190c15382b443762abc06137932b5
Arne <a.chr.hansen@gmx.de>
2014-03-27 22:17:23 +0100

    add colors to script

--- bash_script/change_colorscheme.sh
+++ bash_script/change_colorscheme.sh
@@ -2,7 +2,10 @@
 
 cp style.css style_old.css
 new_background_color="#008373"
-old_green_1=""
-old_green_2=""
-sed -i "470;3596;3583;2692s/#000/$new_background_color" style.css
-
+old_green_1="#24890D"
+new_color_1=""
+old_green_2="#41A62A"
+new_color_1=""
+sed -i "470;3596;3583;2692s/#000/$new_background_color/" style.css
+sed -i "s/$old_green_1/$new_color_1/" style.css
+sed -i "s/$old_green_2/$new_color_2/" style.css
