6a7c9ee6c14a4335683d461254ebe86ecf2214e7
Arne <a.chr.hansen@gmx.de>
2014-03-31 11:42:29 +0200

    introduce arrays

--- bash_script/change_colorscheme.sh
+++ bash_script/change_colorscheme.sh
@@ -6,20 +6,19 @@ line=$(($1+2))
 new_colors=( $( sed 's/,//g;'"$line"'q;d' colorschemes.csv ) )
 old_colors=( $( sed 's/,//g;1q;d' colorschemes.csv ) )
 
-
 # set background colors
 
 ## set colors for header 
-sed -i "833s/#000/$new_background_color/" style.css
+sed -i "833s/${old_colors[2]}/${new_colors[2]}/" style.css
 
 ## set colors for left column
-sed -i "3584s/#000/$new_background_color/" style.css
+sed -i "3584s/${old_colors[2]}/${new_colors[2]}/" style.css
 #sed -i "3597/transparent/$new_background_color/" style.css
 
 ## set color for footer
-sed -i "2693s/#000/$new_background_color/" style.css
+sed -i "2693s/${old_colors[2]}/${new_colors[2]}/" style.css
 
 # set colors for links and highlighted content
-sed -i "s/$old_green_1/$new_color_1/" style.css
-sed -i "s/$old_green_2/$new_color_2/" style.css
-sed -i "s/$old_green_3/$new_color_3/" style.css
+sed -i "s/${old_colors[3]}/${new_colors[3]}/" style.css
+sed -i "s/${old_colors[4]}/${new_colors[4]}/" style.css
+sed -i "s/${old_colors[5]}/${new_colors[5]}/" style.css
